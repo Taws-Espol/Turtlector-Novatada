8a486030920bc60e95f621c952d104099f9b5d14
Braulio Rivas <brauliorivas2004@gmail.com>
2026-05-05 13:43:04 -0500

    move micro

--- frontend/src/features/turtle/components/VoiceMic3D.tsx
+++ frontend/src/features/turtle/components/VoiceMic3D.tsx
@@ -10,9 +10,9 @@ type Props = {
 }
 
 const MIC_LAYOUT_BY_MODE: Record<XRMode, [number, number, number]> = {
-  [xrModes.desktop]: [1.8, -0.8, 0.7],
-  [xrModes.vr]: [0.95, -0.55, -1.55],
-  [xrModes.ar]: [0.85, -0.45, -0.85],
+  [xrModes.desktop]: [0, 0.85, 0.3],
+  [xrModes.vr]: [0, 0.55, -1.7],
+  [xrModes.ar]: [0, 0.52, -1.0],
 }
 
 export default function VoiceMic3D({ xrMode, isListening, isDisabled, onToggle }: Props) {
@@ -47,17 +47,17 @@ export default function VoiceMic3D({ xrMode, isListening, isDisabled, onToggle }
       }}
     >
       <mesh>
-        <capsuleGeometry args={[0.09, 0.25, 8, 16]} />
+        <capsuleGeometry args={[0.13, 0.34, 8, 18]} />
         <meshStandardMaterial color={color} emissive={emissive} emissiveIntensity={0.45} />
       </mesh>
 
-      <mesh position={[0, -0.28, 0]}>
-        <boxGeometry args={[0.08, 0.2, 0.08]} />
+      <mesh position={[0, -0.36, 0]}>
+        <boxGeometry args={[0.11, 0.26, 0.11]} />
         <meshStandardMaterial color={color} emissive={emissive} emissiveIntensity={0.35} />
       </mesh>
 
-      <mesh position={[0, -0.43, 0]}>
-        <torusGeometry args={[0.14, 0.03, 10, 28]} />
+      <mesh position={[0, -0.57, 0]}>
+        <torusGeometry args={[0.2, 0.04, 12, 30]} />
         <meshStandardMaterial color={color} emissive={emissive} emissiveIntensity={0.35} />
       </mesh>
     </group>
